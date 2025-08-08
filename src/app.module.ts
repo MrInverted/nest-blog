5d1a99cb137cf1041c9bd189bd790c90eab3a146
MrInverted <akson.dyuk@gmail.com>
2025-08-08 16:08:45 +0300

    serve static

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,18 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "uploads"),
+      serveRoot: "/uploads"
+    }),
     AuthModule,
     PrismaModule,
     UserModule,
